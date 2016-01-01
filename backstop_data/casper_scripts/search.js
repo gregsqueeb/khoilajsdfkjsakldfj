7d1530b66f37678ad99cea49eec894e130ae2165
Greg Mathews <gregdmathews@gmail.com>
2016-01-01 11:46:24 -0800

    Everything should be working now with random numbers

--- backstop_data/casper_scripts/search.js
+++ backstop_data/casper_scripts/search.js
@@ -1,52 +1,67 @@
-var TAGS = ['240sx'];
-
 module.exports = function(casper, scenario, vp) {
 
-  // for each ( tag in TAGS ){
-    // search('240sx');
+  // for each tag, search the term, and then like all the photos on the page
+  // for ( tag in TAGS ){
+  //   search("#"+TAGS[tag], likePhotos);
   // }
-  // console.log(casper.getHTML('#react-root'));
+  likePhotos();
+};
 
-  // Get all Links
+function likePhotos(){
+
+  // Get all Links to the pictures
   casper.getElementsInfo('a').map(function(info){
-    // Only click it if it is an image
-    casper.wait(500, function(){
+
+    var randomNum = Math.floor((Math.random() * 1000) + 1000);
+    // Only click it if it is an image, images have a url like /p/123456
+    casper.wait(randomNum, function(){
       if(info.attributes.href.indexOf('/p/') > -1){
-        // console.log(JSON.stringify(info, null, 4))
+
+        // Click one of the images to get the picture modal to come up
         casper.click('a[href = "' + info.attributes.href + '" ]');
+
+        randomNum = Math.floor((Math.random() * 2000) + 500);
         // When the modal is up, click the like button if it has not been liked already
-        casper.wait(500, function(){
+        casper.wait(randomNum, function(){
           if(casper.exists('.coreSpriteHeartOpen')){
-            console.log("There is a heart to click")
-            casper.wait(500, function(){
-              // console.log(casper.getHTML('.coreSpriteHeartOpen'));
+            randomNum = Math.floor((Math.random() * 1000) + 500);
+            casper.wait(randomNum, function(){
               casper.click('.coreSpriteHeartOpen');
-              casper.wait(500, function(){
-                // console.log(casper.getHTML('div[role="dialog"]'));
-                if(casper.exists('div[role="dialog"]')){
-                  casper.click('div[role="dialog"]');
-                }
-              });
+              console.log("Clicking like button")
             });
           }
+
+          randomNum = Math.floor((Math.random() * 500) + 500);
+          // Click the modal background to close the modal
+          casper.wait(randomNum, function(){
+            if(casper.exists('div[role="dialog"]')){
+              casper.click('div[role="dialog"]');
+            }
+          });
+
         });
       }
     });
   });
 };
 
-function search(tag){
-  // casper.fillSelectors('form', {
-  //   'input[placeholder = Search]': tag
-  // }, true)
-  casper.click('input[placeholder = Search]');
-  casper.wait(500);
-  casper.sendKeys('input[placeholder = Search]', ""+tag);
-  casper.wait(500);
-  // console.log(casper.getHTML('body'));
-  // document.querySelector("input[placeholder='Search']");
-  // casper.sendKeys('input[placeholder = Search]', casper.page.event.key.Enter, {keepFocus: true});
-  casper.sendKeys('input[placeholder = Search]', casper.page.event.key.Enter);
 
-  casper.wait(500);
-}
+function search(tag, likePhotos){
+
+  //click the search box at the top of the page
+  casper.click('input[placeholder = Search]');
+
+  // type in the tag
+  casper.sendKeys('input[placeholder = Search]', ""+tag);
+
+  // Press enter key
+  casper.wait(500, function(){
+    casper.sendKeys('input[placeholder = Search]', casper.page.event.key.Enter);
+  });
+
+  // Output Title of the page
+  console.log('On page ' + casper.getTitle());
+
+  // The page is now the correct tag, so like the photos
+  likePhotos();
+};
